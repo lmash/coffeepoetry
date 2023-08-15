e4358fd30833028111dc7911aca81eb116487dec
mashie <lmachet@yahoo.com>
2023-08-15 11:30:14 +0100

    Fix image displaying in the Index page
    Prior to this change, images were not displaying correctly
    
    This change adds get_media_prefix to the html template and also adds
    static to the url. This resolves the issue
    
    https://stackoverflow.com/questions/45865205/imagefield-django-template

--- coffee/urls.py
+++ coffee/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
+
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="index"),
@@ -10,4 +13,5 @@ urlpatterns = [
 
     path("new", views.new_cafe, name="new"),
     path("cafe/<int:cafe_id>", views.cafe_view, name="cafe"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
